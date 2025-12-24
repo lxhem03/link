6c60040579010e4e9b41bef084378815968c7e02
Levhcwm21. <alexags1983@gmail.com>
2025-12-24 21:00:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,16 +2,16 @@ from os import environ, getenv
 from Script import script
 
 # 🚀 __Bot Configuration__
-SESSION = environ.get('SESSION', 'RexBots')  # Session name
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+SESSION = environ.get('SESSION', 'filetolin')  # Session name
+API_ID = int(environ.get('API_ID', '23340285'))
+API_HASH = environ.get('API_HASH', 'ab18f905cb5f4a75d41bb48d20acfa50')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
 # 👑 __Owner & Admins__
 ADMINS = [int(i) for i in environ.get('ADMINS', '').split()]
 AUTH_CHANNEL = [int(i) for i in environ.get("AUTH_CHANNEL", "").split()]
-OWNER_USERNAME = environ.get("OWNER_USERNAME", 'RexBots_Official')
-BOT_USERNAME = environ.get("BOT_USERNAME", 'RexBots_Official')
+OWNER_USERNAME = environ.get("OWNER_USERNAME", 'Itsme123c')
+BOT_USERNAME = environ.get("BOT_USERNAME", '')
 
 # 🔗 __Channel & Support Links__
 CHANNEL = environ.get('CHANNEL', 'https://t.me/RexBots_Official')
@@ -28,8 +28,8 @@ SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", ""))
 
 # ✅ __Feature Toggles__
 VERIFY = False  # Enable user verification
-FSUB = environ.get("FSUB", True)  # Force Subscribe
-ENABLE_LIMIT = environ.get("ENABLE_LIMIT", True)
+FSUB = environ.get("FSUB", False)  # Force Subscribe
+ENABLE_LIMIT = environ.get("ENABLE_LIMIT", False)
 BATCH_VERIFY = False
 IS_SHORTLINK = False
 MAINTENANCE_MODE = environ.get("MAINTENANCE_MODE", False)
@@ -62,12 +62,12 @@ PING_INTERVAL = int(environ.get("PING_INTERVAL", 1200))
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', 60))
 RATE_LIMIT_TIMEOUT = int(environ.get("RATE_LIMIT_TIMEOUT", 600))
 MAX_FILES = int(environ.get("MAX_FILES", 50))
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 60))  # Hours
+VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 0))  # Hours
 
 # ⚙️ __Worker & App Config__
 WORKERS = int(environ.get('WORKERS', 10))
 MULTI_CLIENT = False
-NAME = environ.get('name', 'rexbots_official')
+NAME = environ.get('name', 'nectar')
 
 # 🌐 __Web Server__
 ON_HEROKU = 'DYNO' in environ
@@ -78,7 +78,7 @@ NO_PORT = str(environ.get("NO_PORT", "true")).lower() in ("true", "1", "yes")
 HAS_SSL = str(environ.get("HAS_SSL", "true")).lower() in ("true", "1", "yes")
 
 # URL Generation
-BIND_ADDRESS = environ.get("WEB_SERVER_BIND_ADDRESS", "")   ##without https:// paste the base url here 
+BIND_ADDRESS = environ.get("WEB_SERVER_BIND_ADDRESS", "improved-casi-tg-guy-7034d8e8.koyeb.app/")   ##without https:// paste the base url here 
 FQDN = environ.get("FQDN", BIND_ADDRESS)
 
 if not FQDN.startswith("http"):
